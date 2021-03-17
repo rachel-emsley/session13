7294e5d30815db82c0f1fee2496e2630f4d505c8
rachel-emsley <78801984+rachel-emsley@users.noreply.github.com>
2021-03-17 20:47:13 +0000

    add iteration to arrays.js

--- arrays.js
+++ arrays.js
@@ -18,3 +18,8 @@ console.log(favouriteThings)
 // we can to arrays
 favouriteThings.push('cats');
 console.log(favouriteThings);
+
+// iterating through arrays
+for (let i = 0; i < rainbowColours.length; i++) {
+    console.log(rainbowColours[i]);
+}
