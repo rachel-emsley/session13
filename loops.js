677daa263e2c4d22b77484987ee4a45b0567dfde
rachel-emsley <78801984+rachel-emsley@users.noreply.github.com>
2021-03-17 20:18:34 +0000

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,1 +1,33 @@
+// while loop
+var bottlesOfBeer = 99;
 
+while (bottlesOfBeer > 0) {
+    console.log(bottlesOfBeer + 'bottles of beer on the wall');
+    bottlesOfBeer = bottlesOfBeer -1
+}
+
+// for loop
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// for loop with additional logic
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log('fizz');
+    } else if (i % 5 === 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+}
+
+// using break to exit a loop
+for (var i = 1; i <= 100; i++) {
+    console.log('Testing ' + i);
+
+    if (i % 7 === 0) {
+        console.log('Found it!' + i);
+        break;
+    }
+}
